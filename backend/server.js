b2934d0b370ea7adfe9aaff5fd86a59a04c9fe1d
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-05-09 13:10:37 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -37,11 +37,11 @@ app.use("/api/message", messageRoutes);
 
 const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "client/frontend/build")));
+  app.use(express.static(path.join(__dirname1, "frontend/build")));
 
   app.get("*", (req, res) =>
     res.sendFile(
-      path.resolve(__dirname1, "client/frontend", "build", "index.html")
+      path.resolve(__dirname1, "frontend", "build", "index.html")
     )
   );
 } else {
